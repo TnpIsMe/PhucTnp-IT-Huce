ba1126797645c202837ea47fed9d35906a0071ac
Trần Phúc <0400069@st.huce.edu.vn>
2025-10-29 16:15:33 +0700

    Add files via upload

---
+++ HotelManager/src/main/java/model/base/Managerable.java
@@ -0,0 +1,14 @@
+package model.base;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author ThinkPad X1 Gen 9
+ */
+public interface Managerable {
+    
+}
